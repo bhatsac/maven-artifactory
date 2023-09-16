a6d057641b54adbebec188de106c55e62db71c35
bhatsac <bhatsachin87@gmail.com>
2023-09-16 10:46:09 +0530

    initial commit

---
+++ src/main/java/com/sericulture/kar/nic/in/App.java
@@ -0,0 +1,13 @@
+package com.sericulture.kar.nic.in;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
